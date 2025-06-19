ed43d2a65ad1ddcac40fe22545d420d53aa79a1d
Nurgül Yalman <123768429+codeby-nurgul@users.noreply.github.com>
2025-06-19 13:28:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Python in Docker!"
+    return "Hello from main branch!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
